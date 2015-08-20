381944a52e4e463c5ec5d12002efb19cfb7217ef
Ludovico Russo <ludus.russo@gmail.com>
2015-08-20 18:25:36 +0200

    rm migrate dep

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 from app import create_app # , db
 # from app.models import User, Role
 from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
+# from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
