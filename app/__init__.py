fc67037c3b67015200dd22c8840e42a97149c655
Ludovico Russo <ludus.russo@gmail.com>
2015-08-20 18:22:00 +0200

    rm unused modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template 
 from flask.ext.bootstrap import Bootstrap 
 # from flask.ext.mail import Mail
-from flask.ext.moment import Moment
-from flask.ext.sqlalchemy import SQLAlchemy 
+# from flask.ext.moment import Moment
+# from flask.ext.sqlalchemy import SQLAlchemy 
 from config import config
 
 bootstrap = Bootstrap()
-#mail = Mail()
-moment = Moment()
-db = SQLAlchemy()
+# mail = Mail()
+# moment = Moment()
+# db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__) 
@@ -16,8 +16,8 @@ def create_app(config_name):
     config[config_name].init_app(app)
     bootstrap.init_app(app)
     #mail.init_app(app)
-    moment.init_app(app)
-    db.init_app(app)
+    # moment.init_app(app)
+    # db.init_app(app)
 
     from .main import main as main_bp
     app.register_blueprint(main_bp)
