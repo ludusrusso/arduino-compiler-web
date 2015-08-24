73f1de2b59cb37f9fe9462709a3f0789a34b1e23
Ludovico Russo <ludus.russo@gmail.com>
2015-08-24 22:09:57 +0200

    start setting up stuffs for introduce models

--- config.py
+++ config.py
@@ -4,9 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string' 
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>' 
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     
     @staticmethod
     def init_app(app): 
@@ -14,11 +11,6 @@ class Config:
     
 class DevelopmentConfig(Config): 
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
--- manage.py
+++ manage.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import os
-from app import create_app # , db
-# from app.models import User, Role
+from app import create_app, db
+from app.models import Sketch
 from flask.ext.script import Manager, Shell
-# from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 def make_shell_context():
 	return dict(app=app) # , db=db, User=User, Role=Role)
